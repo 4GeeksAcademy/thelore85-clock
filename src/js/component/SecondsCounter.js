1e9739d8ae11a0a9cdb20639006dcbb6b7c8fa1d
TheLore85 <garofalo.digital@gmail.com>
2023-11-07 01:07:56 +0100

    fix: button start/stop + reset

--- src/js/component/SecondsCounter.js
+++ src/js/component/SecondsCounter.js
@@ -5,11 +5,24 @@ import React, { useEffect, useState } from 'react'
 export default function SecondsCounter() {
 
   const[start, setStart] = useState(false)
+  const[status, setStatus] = useState(false)
   const[counter, setCounter] = useState(0)
 
   let num1 = (counter % 10)
   let num2 =  Math.floor(counter /10) %10
   let num3 = Math.floor(counter /100) %10
+
+  const handleButton = () => {
+    if(start){
+      setStart(false)
+      setStatus(false)
+    }
+    else if(!start){
+      setStart(true)
+      setStatus(true)
+    }
+  }
+
 
 
   useEffect(()=>{
@@ -22,7 +35,6 @@ export default function SecondsCounter() {
     },[start])
 
 
-
 	return (
       <section className=" min-vh-100 bg-dark d-flex justify-content-center align-items-center">
         <div className="container text-center">
@@ -32,8 +44,13 @@ export default function SecondsCounter() {
           <div className=" d-inline-block bg-black rounded p-5 m-2 text-white h1 fw-bolder"><p>{num2}</p></div>
           <div className=" d-inline-block bg-black rounded p-5 m-2 text-white h1 fw-bolder"><p>{num1}</p></div>
           <div className="p-4">
-            <button className="btn btn-success me-4" onClick={() => setStart(true)} > START</button>
-            <button className="btn btn-danger" onClick={() => setStart(false)} > STOP</button>
+
+          {!status ? 
+            (<button className="btn btn-success me-4" onClick={ handleButton} > START</button> ):
+            (<button className="btn btn-danger me-4" onClick={ handleButton} > STOP</button>)
+          }
+
+            <button className="btn btn-light text-primary" onClick={() => {setCounter(0)}} > RESET</button>
           </div>
 
         </div>
